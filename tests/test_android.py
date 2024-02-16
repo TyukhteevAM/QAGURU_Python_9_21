59d3091286d02b7fc90c389e9299c7e3d3863a2b
Alexey Tyukhteev <discofan@yandex.ru>
2024-02-16 12:16:43 +0300

    add results.first.click() to test_android.pyandroidpy

--- tests/test_android.py
+++ tests/test_android.py
@@ -23,4 +23,5 @@ def test_android_search_wiki_kremlin(mobile_management_android):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Kremlin'))
+    with step('Open content'):
         results.first.click()
